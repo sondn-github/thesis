200b84b96633cc0eaa26e21c5b0cc2cea79a6d3e
SonDuong97 <duongson29111997@gmail.com>
2020-03-16 15:40:07 +0700

    creating template layouts

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,4 @@
+@include('layouts.partials.head')
+@include('layouts.partials.header')
+@yield('content')
+@include('layouts.partials.foot')
---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Academics &mdash; Website by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset("fonts/icomoon/style.css")}}">
+
+    <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("css/jquery-ui.css")}}">
+    <link rel="stylesheet" href="{{asset("css/owl.carousel.min.css")}}">
+    <link rel="stylesheet" href="{{asset("css/owl.theme.default.min.css")}}">
+    <link rel="stylesheet" href="{{asset("css/owl.theme.default.min.css")}}">
+
+    <link rel="stylesheet" href="{{asset("css/jquery.fancybox.min.css")}}">
+
+    <link rel="stylesheet" href="{{asset("css/bootstrap-datepicker.css")}}">
+
+    <link rel="stylesheet" href="{{asset("fonts/flaticon/font/flaticon.css")}}">
+
+    <link rel="stylesheet" href="{{asset("css/aos.css")}}">
+    <link href="{{asset("css/jquery.mb.YTPlayer.min.css")}}" media="all" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="{{asset("css/style.css")}}">
+
+
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <div class="site-wrap">
