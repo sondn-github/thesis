e8e8d32f691a3f69e773c784c930f265eeb3207f
SonDuong97 <duongson29111997@gmail.com>
2020-07-05 09:48:50 +0700

    comment is_pfs

--- resources/views/admin/criteria/index.blade.php
+++ resources/views/admin/criteria/index.blade.php
@@ -38,16 +38,19 @@
                                     @endforeach
                                 </select>
                             </div>
-                            <div class="form-group">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="is_pfr" id="inlineRadio1" value="1" @if($type->is_pfr) checked @endif>
-                                    <label class="form-check-label" for="inlineRadio1">Đánh giá theo kiểu PFR</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="is_pfr" id="inlineRadio2" value="0" @if(!$type->is_pfr) checked @endif>
-                                    <label class="form-check-label" for="inlineRadio2">Đánh giá theo kiểu truyền thống</label>
-                                </div>
-                            </div>
+                            <input type="hidden" name="is_pfr" id="inlineRadio1" value="1">
+{{--                            <div class="form-group">--}}
+{{--                                <div class="form-check form-check-inline">--}}
+{{--                                </div>--}}
+{{--                                <div class="form-check form-check-inline">--}}
+{{--                                    <input class="form-check-input" type="radio" name="is_pfr" id="inlineRadio1" value="1" @if($type->is_pfr) checked @endif>--}}
+{{--                                    <label class="form-check-label" for="inlineRadio1">Đánh giá theo kiểu PFR</label>--}}
+{{--                                </div>--}}
+{{--                                <div class="form-check form-check-inline">--}}
+{{--                                    <input class="form-check-input" type="radio" name="is_pfr" id="inlineRadio2" value="0" @if(!$type->is_pfr) checked @endif>--}}
+{{--                                    <label class="form-check-label" for="inlineRadio2">Đánh giá theo kiểu truyền thống</label>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                             <button class="btn btn-primary" type="submit">{{__('criteria.apply')}}</button>
                         </form>
                     </div>
