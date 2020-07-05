cbdddc5266bf64df301f998167467b912e08fe5c
SonDuong97 <duongson29111997@gmail.com>
2020-07-05 16:10:03 +0700

    change min number evaluation

--- app/Evaluation.php
+++ app/Evaluation.php
@@ -18,7 +18,7 @@ class Evaluation extends AppModel
     //Constant
     public const TYPE_PFR = 1;
     public const TYPE_NORMAL = 2 ;
-    public const MIN_NUMBER_EVALUATION = 0;
+    public const MIN_NUMBER_EVALUATION = 3;
 
     protected $fillable = [
         self::COL_USER_ID,
